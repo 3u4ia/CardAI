2907161be767d1253b64d054a7733d6eaf8f9dc7
3u4ia <beleteelroi@gmail.com>
2025-04-12 19:10:15 -0500

    Modified uploadtogemini

--- backend/service/UploadToGemini.js
+++ backend/service/UploadToGemini.js
@@ -5,7 +5,19 @@ const uploadFile = async (body) => {
     const { imageArray } = body;
 
 
-    
+    const imageParts = body.map(file => ({
+        inlineData: {
+            data: file.buffer.toString('base64'),
+            mimeType: file.mimeType
+        }
+    }));
+
+    const prompParts = [
+        { text: "Give a small notecard summary of these images"},
+        ...imageParts
+    ];
+
+    const result = await chat.generateContent
     
 
 }
